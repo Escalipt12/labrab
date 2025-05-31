91a297c8c75195974ef59e75a18764a35b47795c
Escalipt12 <escalipt@gmail.com>
2025-05-31 01:13:16 +0000

    Update main.c

--- lab14/main.c
+++ lab14/main.c
@@ -1,43 +1,45 @@
+#define  _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
 #include <setjmp.h>
+#include <limits.h>
 
-// Global variable for storing jump context
-jmp_buf jump_buffer;
+jmp_buf _jump_buffer;
 
-// Global variable for storing sum
 unsigned long long sum = 0;
 
-// Recursive function for calculating Fibonacci numbers and their sum
-void fibonacci(unsigned long long a, unsigned long long b, int n) {
-    // If we reached the required number of terms, perform non-local jump
-    if (n == 0) {
-        longjmp(jump_buffer, 1);
-    }
-    
-    // Add current number to sum
-    sum += a;
-    
-    // Recursive call for next Fibonacci number
-    fibonacci(b, a + b, n - 1);
+void fibonacci(unsigned long long a, unsigned long long b, int c) {
+	if (c == 0) {
+		longjmp(_jump_buffer, 1); 
+	}
+
+	if (b > ULLONG_MAX - a) {
+		printf("Too big. Get lost!\n");
+		longjmp(_jump_buffer, 2); 
+	}
+
+	sum += a;
+	fibonacci(b, a + b, c - 1);
 }
 
 int main() {
-    int n;
-    printf("Enter the number of Fibonacci numbers to sum: ");
-    scanf("%d", &n);
-    
-    if (n <= 0) {
-        printf("Please enter a positive number.\n");
-        return 1;
-    }
-    
-    // Set the return point
-    if (setjmp(jump_buffer) == 0) {
-        // Start calculation with first two Fibonacci numbers (1, 1)
-        fibonacci(1, 1, n);
-    }
-    
-    printf("Sum of first %d Fibonacci numbers: %llu\n", n, sum);
-    
-    return 0;
-} 
+	int n = 0;
+
+	printf("Enter the number of Fibonacci numbers to sum: ");
+	scanf("%d", &n);
+
+	if (n <= 0) {
+		printf("Please enter a positive number.\n");
+		return 1;
+	}
+
+	int jump_code = setjmp(_jump_buffer);
+	if (jump_code == 0) {
+		fibonacci(1, 1, n);
+	}
+	else if (jump_code == 1) {
+		printf("Sum of first %d Fibonacci numbers: %llu\n", n, sum);
+	}
+
+	return 0;
+}
